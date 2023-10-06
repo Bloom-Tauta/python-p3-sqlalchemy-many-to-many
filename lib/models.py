a93dca46672bdb028180244e7ce4d7170828e4b7
Bloom <bloomtauta@gmail.com>
2023-10-07 01:35:30 +0300

    done

--- lib/models.py
+++ lib/models.py
@@ -7,8 +7,15 @@ convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 }
 metadata = MetaData(naming_convention=convention)
-
 Base = declarative_base(metadata=metadata)
+
+game_user = Table(
+    'game_users',
+    Base.metadata,
+    Column('game_id', ForeignKey('games.id'), primary_key=True),
+    Column('user_id', ForeignKey('users.id'), primary_key=True),
+    extend_existing=True,
+)
 
 class Game(Base):
     __tablename__ = 'games'
@@ -18,8 +25,11 @@ class Game(Base):
     genre = Column(String())
     platform = Column(String())
     price = Column(Integer())
+    created_at = Column(DateTime(), server_default=func.now())
+    updated_at = Column(DateTime(), onupdate=func.now())
 
     reviews = relationship('Review', backref=backref('game'))
+    users = relationship('User', secondary=game_user, back_populates='games')
 
     def __repr__(self):
         return f'Game(id={self.id}, ' + \
@@ -32,10 +42,45 @@ class Review(Base):
     id = Column(Integer(), primary_key=True)
     score = Column(Integer())
     comment = Column(String())
+    created_at = Column(DateTime(), server_default=func.now())
+    updated_at = Column(DateTime(), onupdate=func.now())
     
     game_id = Column(Integer(), ForeignKey('games.id'))
+    user_id = Column(Integer(), ForeignKey('users.id'))
 
     def __repr__(self):
         return f'Review(id={self.id}, ' + \
             f'score={self.score}, ' + \
             f'game_id={self.game_id})'
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    created_at = Column(DateTime(), server_default=func.now())
+    updated_at = Column(DateTime(), onupdate=func.now())
+    reviews = relationship('Review', backref=backref('user'))
+
+    games = relationship('Game', secondary=game_user, back_populates="users")
+    reviews = relationship('Review', backref=backref('user'))
+
+    def __repr__(self):
+        return f'User(id={self.id}, ' + \
+            f'name={self.name})'
+
+
+# class GameUser(Base):
+#     __tablename__ = 'game_users'
+
+#     id = Column(Integer(), primary_key=True)
+#     game_id = Column(ForeignKey('game.id'))
+#     user_id = Column(ForeignKey('user.id'))
+
+#     game = relationship('Game', back_populates='game_users')
+#     user = relationship('User', back_populates='game_users')
+
+#     def __repr__(self):
+#         return f'GameUser(game_id={self.game_id}, ' + \
+#             f'user_id={self.user_id})'
